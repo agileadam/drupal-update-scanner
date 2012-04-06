542ca98ce2a492af4e5df2e55ba25afdaa24bb29
Adam Courtemanche <adam.courtemanche@gmail.com>
2012-04-06 16:12:54 -0400

    Switch to argparse; add --dir argument

--- drupal_update_scanner.py
+++ drupal_update_scanner.py
@@ -4,21 +4,23 @@ import os
 import subprocess
 import smtplib
 from email.mime.text import MIMEText
-from optparse import OptionParser
+import argparse
 
-parser = OptionParser()
-parser.add_option("-f", "--file", dest="filename",
+parser = argparse.ArgumentParser(description='Scan for Drupal updates.')
+parser.add_argument("-d", "--dir", dest="scandir", required=True,
+                  help="which directory contains your drupal directories?", metavar="SCAN_DIRECTORY")
+parser.add_argument("-f", "--file", dest="filename",
                   help="write report to specific FILE", metavar="FILE")
-parser.add_option("-m", "--mail", dest="mailaddresses",
+parser.add_argument("-m", "--mail", dest="mailaddresses",
                   help="send report to email address or addresses \
                   e.g., --mail \"foo@foo.com,bar@bar.com\"", metavar="ADDRESSES")
-parser.add_option("-s", "--subject", dest="mailsubject", default="UPDATE REPORT",
+parser.add_argument("-s", "--subject", dest="mailsubject", default="UPDATE REPORT",
                   help="set email subject e.g., --subject=\"Update Alert!\"",
                   metavar="\"SUBJECT\"")
-parser.add_option("-q", "--quiet",
+parser.add_argument("-q", "--quiet",
                   action="store_false", dest="verbose", default=True,
                   help="do not show output")
-(options, args) = parser.parse_args()
+args = parser.parse_args()
 
 # Execute a bash command and return the stdout
 def runBash(cmd):
@@ -26,56 +28,68 @@ def runBash(cmd):
     out = p.stdout.read().strip()
     return out
 
+TEMPFILE = '/tmp/allupdates.txt'
+
 # We need to write to a temp file if -m OR -f are used!
-if options.mailaddresses or options.filename:
-    TEMPFILE = '/tmp/allupdates.txt'
+if args.mailaddresses or args.filename:
     if os.path.exists(TEMPFILE): 
         os.remove(TEMPFILE)
     f = open(TEMPFILE, 'w')
 
-if options.filename:
-    if os.path.exists(options.filename): 
+if args.filename:
+    if os.path.exists(args.filename):
         sys.exit("The file specified already exists!")
+
+# Store the directory from which the user is executing this script
+# so we can store a file (if they use -f/--file) relative to this directory
+origdir = os.getcwd()
+
+# Move to the directory that contains the Drupal sites
+os.chdir(args.scandir)
+
 root, dirs, files = os.walk('.').next()
-workingdir = os.getcwd()
 
 for name in dirs:
     os.chdir(name)
     if os.path.exists('modules'):
-        if options.verbose:
+        if args.verbose:
             print name
         results = runBash('drush pm-update --simulate | grep "UPDATE\|not supported"')
         if results:
-            if options.verbose:
+            if args.verbose:
                 print results
-            if options.mailaddresses or options.filename:
+            if args.mailaddresses or args.filename:
                 f.write("###################################\n")
                 f.write(name + "\n " + results.replace("\r\n", "\n") + "\n\n")
         else:
-            if options.verbose:
+            if args.verbose:
                 print "No updates found"
-    os.chdir(workingdir)
+    os.chdir(args.scandir)
 
-f.close()
-
-if options.mailaddresses:
-    print "Sending email to %s" % options.mailaddresses
-
-    f = open(TEMPFILE, 'rb')
-    msg = MIMEText(f.read())
+if args.mailaddresses or args.filename:
     f.close()
 
-    msg['Subject'] = options.mailsubject
+# Move back to where the user started
+os.chdir(origdir)
+
+if args.mailaddresses:
+    print "Sending email to %s" % args.mailaddresses
+
+    f = open(TEMPFILE, 'rb')
+    msg = MIMEText(f.read(), 'plain')
+    f.close()
+
+    msg['Subject'] = args.mailsubject
     msg['From'] = 'adam@transitid.com'
-    msg['To'] = options.mailaddresses
+    msg['To'] = args.mailaddresses
 
     # This requires that a local SMTP server is running (e.g., postfix)
     s = smtplib.SMTP('localhost')
-    s.sendmail('adam@transitid.com', [options.mailaddresses], msg.as_string())
+    s.sendmail('adam@transitid.com', [args.mailaddresses], msg.as_string())
     s.quit()
 
-if options.filename:
-    os.system("mv %s %s" % (TEMPFILE, options.filename))
+if args.filename:
+    os.system("mv %s %s" % (TEMPFILE, args.filename))
 
 # Just in case the user doesn't want this hanging around
 if os.path.exists(TEMPFILE): 
