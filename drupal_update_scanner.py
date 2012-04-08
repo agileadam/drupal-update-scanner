e5379d1b527043f910fea24ec2ae87d99caeed94
Adam Courtemanche <adam.courtemanche@gmail.com>
2012-04-07 23:05:54 -0400

    Breaks out processing into function; glob-based scanning

--- drupal_update_scanner.py
+++ drupal_update_scanner.py
@@ -5,6 +5,7 @@ import subprocess
 import smtplib
 from email.mime.text import MIMEText
 import argparse
+import glob
 
 parser = argparse.ArgumentParser(description='Scan for Drupal updates.')
 parser.add_argument("-d", "--dir", dest="scandir", required=True,
@@ -22,11 +23,45 @@ parser.add_argument("-q", "--quiet",
                   help="do not show output")
 args = parser.parse_args()
 
+# Exit as early as possible; if output file
+# already exists, exit with message
+if args.filename:
+    if os.path.exists(args.filename):
+        sys.exit("The file specified already exists!")
+
 # Execute a bash command and return the stdout
 def runBash(cmd):
     p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
     out = p.stdout.read().strip()
     return out
+
+# Process a Drupal directory (dir MUST be a Drupal directory
+# as we're not checking this here!)
+def processDir(dir):
+    os.chdir(dir)
+    if args.verbose:
+        print dir
+
+    drush = subprocess.Popen(['drush', 'pm-update', '--simulate'],
+                             stdout=subprocess.PIPE,
+                             )
+    grep = subprocess.Popen(['grep', '"UPDATE\|not supported"'],
+                             stdin=drush.stdout,
+                             stdout=subprocess.PIPE,
+                             )
+
+    results = grep.stdout.read()
+
+    if results:
+        if args.verbose:
+            print results
+        if args.mailaddresses or args.filename:
+            f.write("###################################\n")
+            f.write(dir + "\n " + results.replace("\r\n", "\n") + "\n\n")
+    else:
+        if args.verbose:
+            print "No updates found"
+    os.chdir(args.scandir)
 
 TEMPFILE = '/tmp/allupdates.txt'
 
@@ -42,10 +77,6 @@ if args.mailaddresses or args.filename:
         os.remove(TEMPFILE)
     f = open(TEMPFILE, 'w')
 
-if args.filename:
-    if os.path.exists(args.filename):
-        sys.exit("The file specified already exists!")
-
 # Store the directory from which the user is executing this script
 # so we can store a file (if they use -f/--file) relative to this directory
 origdir = os.getcwd()
@@ -55,22 +86,11 @@ os.chdir(args.scandir)
 
 root, dirs, files = os.walk('.').next()
 
-for name in dirs:
-    os.chdir(name)
-    if os.path.exists('modules'):
-        if args.verbose:
-            print name
-        results = runBash('drush pm-update --simulate | grep "UPDATE\|not supported"')
-        if results:
-            if args.verbose:
-                print results
-            if args.mailaddresses or args.filename:
-                f.write("###################################\n")
-                f.write(name + "\n " + results.replace("\r\n", "\n") + "\n\n")
-        else:
-            if args.verbose:
-                print "No updates found"
-    os.chdir(args.scandir)
+for name in glob.glob('*/sites/all/modules'):
+    processDir(name.replace('/sites/all/modules', ''))
+
+for name in glob.glob('*/*/sites/all/modules'):
+    processDir(name.replace('/sites/all/modules', ''))
 
 if args.mailaddresses or args.filename:
     f.close()
