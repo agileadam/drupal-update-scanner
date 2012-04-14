37f2cc4b3227ef21a2ead8628e55ee15ff451935
Adam Courtemanche <adam.courtemanche@gmail.com>
2012-04-14 01:31:19 -0400

    Adds more help and comments

--- drupal_update_scanner.py
+++ drupal_update_scanner.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python
+#
+# Cronjob example (single line):
+# /usr/local/bin/python2.7 /usr/local/bin/drupal_update_scanner.py -d /webapps/
+#      -t 1 | mail -s "Drupal updates for Server X" agileadam@gmail.com
+
 import sys
 import os
 import subprocess
 import argparse
 import glob
 
-parser = argparse.ArgumentParser(description='Scan for Drupal updates.')
+parser = argparse.ArgumentParser(description='Scans a server for sites that need Drupal updates, and outputs a report (to screen or file) that shows which updates are required for each site. A popular implemntation is to run this via a cronjob and pipe the command to a mail application to email the output. View the source code for more help!')
 parser.add_argument("-d", "--dir", dest="scandir", required=True,
                   help="which directory contains your drupal sites (you can optionally \
                   traverse deeper from this root directory using the -t/--traverse argument)",
